e057c804b3f6f32a555932122d55f7bb30b473d0
SvitlanaDA <asstomatc2022@gmail.com>
2025-05-29 22:08:04 +0200

    Add 7 files

---
+++ python_and_git_6_7_logic/task_1_num.py
@@ -0,0 +1,19 @@
+print()
+print("First version Code")
+nx = int(input ("Enter a number: "))
+if nx < 10:
+    print("The number ", nx, " is less than 10")
+elif nx > 20:
+    print("The number ", nx, " is greater than 20")
+else:
+    print("The number ", nx, " is between 10 and 20")
+print()
+print("Second version Code")
+ny = int(input ("Enter a number: "))
+if 20 >= ny >= 10:
+    print("The number ", ny, " is between 10 and 20")
+elif ny > 20:
+    print("The ", ny, " is greater than 20")
+else:
+    print("The number ", ny, " is less than 10")
+print("Thanks")
