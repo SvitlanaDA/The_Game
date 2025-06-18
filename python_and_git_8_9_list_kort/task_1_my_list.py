a992a9be3f3e333dfa63b3be979f89a8563c3be2
SvitlanaDA <asstomatc2022@gmail.com>
2025-06-18 19:45:54 +0200

    Renamed and deleted files are committed

--- python_and_git_8_9_list_kort/task_1_my_list.py
+++
@@ -1,6 +0,0 @@
-import random
-
-my_list = list(['Svitlana','Anastasia','Vlad','Kateryna','Nikolay'])
-leader = random.choice(my_list)
-print("Participants: ", my_list)
-print(f"Selected leader: {leader}")
